4d1d794a4051781f48464688b7b075b8b7691d5e
Yordanov <iliyanyyordanov@gmail.com>
2021-09-12 08:00:20 +0100

    Create <Header /> common component with <Navigation /> inside
    Signed-off-by: Yordanov <iliyanyyordanov@gmail.com>

---
+++ src/components/Common/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navigation from '../Navigation';
+
+const Header = () => {
+  return (
+    <div data-testid="header-wrapper">
+      <Navigation />
+    </div>
+  );
+};
+
+export default Header;
