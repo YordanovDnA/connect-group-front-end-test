e5017650eb93b4b4516e84860d1663bb037598c6
Yordanov <iliyanyyordanov@gmail.com>
2021-09-12 08:07:47 +0100

    Update <VehicleList /> to display each vehicle, <Spinner /> on loading, and <Error/> on error, remove redundant code
    Signed-off-by: Yordanov <iliyanyyordanov@gmail.com>

--- src/components/VehicleList/index.js
+++ src/components/VehicleList/index.js
@@ -1,40 +1,42 @@
 import React from 'react';
 import useData from './useData';
 import './style.scss';
+import Spinner from '../Common/Loaders/Spinner/index';
+import Error from '../Common/Notifications/Error/index';
 
 export default function VehicleList() {
   // eslint-disable-next-line no-unused-vars
   const [loading, error, vehicles] = useData();
-
   if (loading) {
-    return <div data-testid="loading">Loading</div>;
+    return (
+      <div data-testid="loading" style={{ minHeight: '80vh', display: 'flex', alignItems: 'center' }}>
+        <Spinner />
+      </div>
+    );
   }
 
   if (error) {
-    return <div data-testid="error">{ error }</div>;
+    return <div data-testid="error" style={{ margin: '0 10px ' }}><Error message={error} /></div>;
   }
 
-  return (
-    <div data-testid="results">
-      <p>List of vehicles will be displayed here</p>
-      <p>
-        Visit
-        <a href="/api/vehicles.json" target="_blank"> /api/vehicles.json</a>
-        {' '}
-        (main endpoint)
-      </p>
-      <p>
-        Visit
-        <a href="/api/vehicle_fpace.json" target="_blank">/api/vehicle_fpace.json</a>
-        {' '}
-        (detail endpoint - apiUrl)
-      </p>
-      <p>
-        Visit
-        <a href="/api/vehicle_xf.json" target="_blank">/api/vehicle_xf.json</a>
-        {' '}
-        (vehicle without any price)
-      </p>
-    </div>
-  );
+  if (vehicles) {
+    return (
+      <div data-testid="results" className="results-wrapper">
+        {vehicles.map((e) => {
+          return (
+            <div key={e.id} className="vehicle-details">
+              <img src={e.media[0].url} alt={`Jaguar ${e.id}.`} />
+              <div className="details">
+                <h2>{`JAGUAR ${e.id.toUpperCase()}`}</h2>
+                <div className="price-wrapper"><span id="price">{`From ${e.details.price}`}</span></div>
+                <div className="details-wrapper">
+                  <p>{e.details.description}</p>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
