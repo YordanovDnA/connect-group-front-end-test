cf3a1048ea04b593a755d66da13ee3d720c9690f
Yordanov <iliyanyyordanov@gmail.com>
2021-09-12 07:54:02 +0100

    Create <Spinner /> common component
    Signed-off-by: Yordanov <iliyanyyordanov@gmail.com>

---
+++ src/components/Common/Loaders/Spinner/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.scss';
+
+const Spinner = () => {
+  return (
+    <div data-testid="spinner" className="spinner" />
+  );
+};
+
+export default Spinner;
