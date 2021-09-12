5a887b0c3b2ac78295981529189bfdfa84842ef4
Yordanov <iliyanyyordanov@gmail.com>
2021-09-12 08:09:15 +0100

    Update tests for <VehicleList /> using the mockup
    Signed-off-by: Yordanov <iliyanyyordanov@gmail.com>

--- src/components/VehicleList/__tests__/VehicleList.test.js
+++ src/components/VehicleList/__tests__/VehicleList.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import VehicleList from '..';
 import useData from '../useData';
+import mockup from '../../../mockups/index.json';
 
 jest.mock('../useData');
 
@@ -25,7 +26,7 @@ describe('<VehicleList /> Tests', () => {
   });
 
   it('Should show results if loading successfully finished', () => {
-    useData.mockReturnValue([false, false, 'results']);
+    useData.mockReturnValue([false, false, mockup.vehicles]);
     const { queryByTestId } = render(<VehicleList />);
 
     expect(queryByTestId('loading')).toBeNull();
